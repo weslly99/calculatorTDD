5f38644fb05b7cbae53d1ff774d47107bcfcd8ff
Weslly Oliveira <weslly99@hotmail.com>
2021-04-28 22:28:06 -0300

    test_sub fail

--- test_calc.py
+++ test_calc.py
@@ -5,3 +5,8 @@ def test_sum():
     calc = Calculator()
     result = calc.sum(3,7)
     assert result == 10
+
+def test_sub():
+    calc = Calculator()
+    result = calc.sub(3,7)
+    assert result == 4
