beb28a0b695f6a5fd750d0b04d8e5e7e31437f55
Weslly Oliveira <weslly99@hotmail.com>
2021-04-30 08:58:44 -0300

    add subtration feature

--- calc.py
+++ calc.py
@@ -4,3 +4,7 @@ class Calculator:
     def sum(self, val1, val2):
         """Calculate the sum of two values """
         return val1 + val2
+    
+    def sub(self, val1, val2):
+        """Calculate the subtration of two values """
+        return val1 - val2
